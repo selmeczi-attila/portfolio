a8a578aed083a017e543654e5815fd79e2ac8a9f
Attila <s.attila.dev@gmail.com>
2025-10-26 19:43:44 +0100

    basic design and 1 project added

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,87 @@
-import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Available from "./components/Available";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
+
+import { faGlobe } from "@fortawesome/free-solid-svg-icons/faGlobe";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons/faEnvelope";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <div className="relative w-full min-h-screen bg-grid-squares">
+      <div className="flex justify-center mb-[5vh]">
+        <div className="flex flex-col items-center justify-center h-fit max-w-100 min-w-75 w-[40vw] mt-[15vh] bg-[#2F2F37] outline-2 outline-[#383842] rounded-md shadow-2xl p-5">
+          <div className="w-20 h-20 mb-3 flex items-center justify-center rounded-full bg-[#4B4B58]">
+            <FontAwesomeIcon icon={faUser} size="3x" />
+          </div>
+          <p className="font-bold">Selmeczi Attila</p>
+          <p className="text-[#9ca3af] font-semibold">Software Engineer</p>
+
+          <p className="text-center text-[#9ca3af] mt-2 px-1">
+            A software engineer who builds{" "}
+            <span className=" text-white underline decoration-dashed">
+              efficient
+            </span>{" "}
+            and{" "}
+            <span className=" text-white underline decoration-dashed">
+              reliable
+            </span>{" "}
+            applications.
           </p>
+
+          <Available isAvailable={false} />
+
+          <div className="space-x-3">
+            <a href="https://github.com/selmeczi-attila" target="_blank">
+              <FontAwesomeIcon
+                className="mt-4 cursor-pointer"
+                icon={faGithub}
+                size="2x"
+              />
+            </a>
+            <a href="mailto:s.attila.dev@gmail.com" target="_blank">
+              <FontAwesomeIcon
+                className="mt-4 cursor-pointer"
+                icon={faEnvelope}
+                size="2x"
+              />
+            </a>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+      </div>
+
+      <h1 className="text-4xl font-bold text-center">Projects</h1>
+
+      <div className="flex justify-center">
+        <div className="flex flex-col items-center justify-center h-fit max-w-100 min-w-75 w-[40vw] mt-[5vh] bg-[#2F2F37] outline-2 outline-[#383842] rounded-md shadow-2xl p-5">
+          <p className="font-bold">CleanCo</p>
+
+          <p className="text-center text-[#9ca3af] mt-3 px-1">
+            A responsive website offering car and carpet cleaning services.{" "}
+            <br></br>
+            Collab project with my friend.
+          </p>
+          <div className="space-x-3">
+            <a
+              href="https://github.com/Geher-Marcell/carpet_cleaning"
+              target="_blank"
+            >
+              <FontAwesomeIcon
+                className="mt-4 cursor-pointer"
+                icon={faGithub}
+                size="2x"
+              />
+            </a>
+            <a href="https://carpet-cleaning-dun.vercel.app/" target="_blank">
+              <FontAwesomeIcon
+                className="mt-4 cursor-pointer"
+                icon={faGlobe}
+                size="2x"
+              />
+            </a>
+          </div>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
